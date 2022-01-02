7e8e1a59168467c3f42b2c023c9db18fc4e0bee3
kouaouci <karimakouaouci2803@gmail.com>
2022-01-02 18:12:52 +0100

    challenge fini

--- src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
@@ -9,63 +9,60 @@ import java.util.List;
 
 public class WizardRepository implements WizardDao {
 
-    private static List<Wizard> wizards = new ArrayList<>(
-            Arrays.asList(
-                    new Wizard(1L, "Harry", "Potter", new Date(80, 6, 31), "London", "", false)
-            )
-    );
+	private static List<Wizard> wizards = new ArrayList<>(
+			Arrays.asList(new Wizard(1L, "Harry", "Potter", new Date(80, 6, 31), "London", "", false)));
 
-    @Override
-    public Wizard save(Wizard wizard) {
+	@Override
+	public Wizard save(Wizard wizard) {
 
-        wizard.setId((long) (wizards.size() + 1));
-        wizards.add(wizard);
-        return wizard;
-    }
+		wizard.setId((long) (wizards.size() + 1));
+		wizards.add(wizard);
+		return wizard;
+	}
 
-    @Override
-    public Wizard findById(Long id) {
+	@Override
+	public Wizard findById(Long id) {
 
-        for (Wizard wizard : wizards) {
-            if (wizard.getId().equals(id)) {
-                return wizard;
-            }
-        }
-        return null;
-    }
+		for (Wizard wizard : wizards) {
+			if (wizard.getId().equals(id)) {
+				return wizard;
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public List<Wizard> findAll() {
+	@Override
+	public List<Wizard> findAll() {
 
-        return wizards;
-    }
+		return wizards;
+	}
 
-    @Override
-    public Wizard update(Wizard wizard) {
+	@Override
+	public Wizard update(Wizard wizard) {
 
-        for (Wizard update : wizards) {
-            if (update.getId().equals(wizard.getId())) {
-                update.setFirstName(wizard.getFirstName());
-                update.setLastName(wizard.getLastName());
-                update.setBirthday(wizard.getBirthday());
-                update.setBirthPlace(wizard.getBirthPlace());
-                update.setBiography(wizard.getBiography());
-                update.setMuggle(wizard.isMuggle());
+		for (Wizard update : wizards) {
+			if (update.getId().equals(wizard.getId())) {
+				update.setFirstName(wizard.getFirstName());
+				update.setLastName(wizard.getLastName());
+				update.setBirthday(wizard.getBirthday());
+				update.setBirthPlace(wizard.getBirthPlace());
+				update.setBiography(wizard.getBiography());
+				update.setMuggle(wizard.isMuggle());
 
-                return update;
-            }
-        }
-        return null;
-    }
+				return update;
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public void deleteById(Long id) {
+	@Override
+	public void deleteById(Long id) {
 
-        for (Wizard wizard : wizards) {
-            if (wizard.getId().equals(id)) {
-                wizards.remove(wizard);
-                break;
-            }
-        }
-    }
+		for (Wizard wizard : wizards) {
+			if (wizard.getId().equals(id)) {
+				wizards.remove(wizard);
+				break;
+			}
+		}
+	}
 }
